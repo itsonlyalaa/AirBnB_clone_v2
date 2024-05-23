7c35f7731dd9ad2d053db5c49c882d33c6d3a529
Alaa Okair <alaaokair330@gmail.com>
2024-05-22 19:20:45 -0700

    tasks

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -38,10 +38,12 @@ def display_number(n):
     """display "n is a number" only if n is an integer"""
     return "{:d} is a number".format(n)
 
+
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def display_html(n):
     """display a HTML page only if "n" is an integer"""
     return render_template('5-number.html', n=n)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
